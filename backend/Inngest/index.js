3ba82784a24a436b5ec2ec5582549420aff29225
Furqan <furqanshabbir234@gmail.com>
2025-07-05 11:46:47 +0300

    variable updated

--- backend/Inngest/index.js
+++ backend/Inngest/index.js
@@ -11,12 +11,12 @@ const syncUserCreation = inngest.createFunction(
     {id:"sync-user-from-clerk"},
     {event:"clerk/user.created"},
     async ({event})=>{
-        const {id , first_name , last_name , email_address , image_url} = event.data;
+        const {id , first_name , last_name , email_addresses , image_url} = event.data;
 
         const userData = {
             _id : id,
             name:`${first_name} ${last_name}`,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             image : image_url
         }
         await userModel.create(userData);
@@ -38,12 +38,12 @@ const syncUserUpdation = inngest.createFunction(
     {id : "update-user-with-clerk"},
     { event : "clerk/user.updated"},
     async ({event})=>{
-        const {id , first_name , last_name , email_address , image_url} = event.data;
+        const {id , first_name , last_name , email_addresses , image_url} = event.data;
         
         const userData = {
             _id : id,
             name:`${first_name} ${last_name}`,
-            email : email_address[0].email_address,
+            email : email_addresses[0].email_address,
             image : image_url
         }
         await userModel.findByIdAndUpdate(id,userData)
